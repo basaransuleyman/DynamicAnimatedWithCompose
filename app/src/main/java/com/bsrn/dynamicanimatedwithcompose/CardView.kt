47a8906252978a10ac1b582c4156338808ca6043
Suleyman Basaranoglu <suleymanbasaranoglu@gmail.com>
2024-02-27 13:52:33 +0300

    Add sample icon path data

--- app/src/main/java/com/bsrn/dynamicanimatedwithcompose/CardView.kt
+++ app/src/main/java/com/bsrn/dynamicanimatedwithcompose/CardView.kt
@@ -3,11 +3,9 @@ package com.bsrn.dynamicanimatedwithcompose
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/bsrn/dynamicanimatedwithcompose/List.kt
+++ app/src/main/java/com/bsrn/dynamicanimatedwithcompose/List.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 
 @Composable
-fun MyRecyclerView(myDataList: List<MyDataType>) {
+fun SampleListView(myDataList: List<MyDataType>) {
     LazyColumn {
         items(myDataList.size) { index ->
             CardView(myDataList[index])
